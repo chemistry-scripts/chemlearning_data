e9654cfcf5af6078566872d0a8d2ef25cc5b2d7c
Emmanuel <emmanuel.nicolas_github@polytechnique.org>
2019-07-11 16:39:42 +0200

    Update chemlearning_data.py

---
+++ chemlearning_data/chemlearning_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, E. Nicolas
+
+"""Tools to use data (especially from QM9) for machine learning applications."""
+
+
+# Here comes your imports
+import cclib
+import tarfile
+
+# Here comes your (few) global variables
+
+# Here comes your class definitions
+
+# Here comes your function definitions
+
+
+def main():
+    """Launcher."""
+    # init the GUI or anything else
+    pass
+
+
+if __name__ == "__main__":
+    main()
